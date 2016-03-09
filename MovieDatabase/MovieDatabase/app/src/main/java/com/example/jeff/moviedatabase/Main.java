2fa7801e9f1ef6558b247839ca50013d2934b5a5
jeffquitevis <jeffquitevis@yahoo.ca>
2016-03-08 18:07:56 -0800

    jeff

--- MovieDatabase/MovieDatabase/app/src/main/java/com/example/jeff/moviedatabase/Main.java
+++ MovieDatabase/MovieDatabase/app/src/main/java/com/example/jeff/moviedatabase/Main.java
@@ -13,14 +13,12 @@ public class Main extends Activity implements MovieListFragment.OnTransferMovieD
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-       detailMovieFragment = (DetailMovieFragment) getFragmentManager().findFragmentById(R.id.detailFragment);
-
-
+        detailMovieFragment = (DetailMovieFragment) getFragmentManager().findFragmentById(R.id.detailFragment);
     }
 
     @Override
     public void setMovieDetails(List<Movie> movieList, int position) {
-    detailMovieFragment.setDetails(movieList[position]);
+    detailMovieFragment.setDetails(movieList.get(position).getOriginalTitle(),movieList.get(position).getReleaseDate(),movieList.get(position).getOverview(),
+            movieList.get(position).getPosterPath(),movieList.get(position).getBackdropPath());
     }
 }
--- MovieDatabase/MovieDatabase/app/src/main/java/com/example/jeff/moviedatabase/MovieListFragment.java
+++ MovieDatabase/MovieDatabase/app/src/main/java/com/example/jeff/moviedatabase/MovieListFragment.java
@@ -139,7 +139,6 @@ public class MovieListFragment extends Fragment {
                     e.printStackTrace();
                 }
             }
-
             return null;
         }
 
